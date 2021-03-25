c74154ba830161b5afb41de30fba7420746ea982
Volodymyr Marynychev <requix@gmail.com>
2021-03-25 19:31:26 +0100

    Deployer policies added

--- terraform/modules/ecs/ecs_iam.tf
+++ terraform/modules/ecs/ecs_iam.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_role" "ecs_instance_role" {
   name = "${var.environment}_ecs_instance_role"
-  assume_role_policy = file("policies/assume_role_policy.json")
+  assume_role_policy = file("../policies/assume_role_policy.json")
 }
 
 resource "aws_iam_instance_profile" "ecs" {
@@ -12,4 +12,19 @@ resource "aws_iam_instance_profile" "ecs" {
 resource "aws_iam_role_policy_attachment" "ecs_ec2_role" {
   role       = aws_iam_role.ecs_instance_role.id
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_user" "ecs_deployer" {
+  name = "ecs_deployer"
+  path = "/ecs/"
+}
+
+resource "aws_iam_user_policy" "ecs_deployer_policy" {
+  name = "ecs_deployer_policy"
+  user = aws_iam_user.ecs_deployer.name
+  policy = file("../policies/ecs_deployer_policy.json")
+}
+
+resource "aws_iam_access_key" "ecs_deployer" {
+  user = aws_iam_user.ecs_deployer.name
 }
